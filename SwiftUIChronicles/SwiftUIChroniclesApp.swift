86f77cbb8e5a17835864cbb0bd5284d31fa3aab9
Aykut Güven <naykutguven@gmail.com>
2025-03-13 14:25:25 +0100

    Add initial project

---
+++ SwiftUIChronicles/SwiftUIChroniclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChroniclesApp.swift
+//  SwiftUIChronicles
+//
+//  Created by Aykut Güven on 13.03.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChroniclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIChroniclesTests/SwiftUIChroniclesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChroniclesTests.swift
+//  SwiftUIChroniclesTests
+//
+//  Created by Aykut Güven on 13.03.25.
+//
+
+import Testing
+@testable import SwiftUIChronicles
+
+struct SwiftUIChroniclesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
