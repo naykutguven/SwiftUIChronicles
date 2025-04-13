c62dc4bcc26c3d79e5acbfcf16abed30aef1ffde
Aykut Güven <naykutguven@gmail.com>
2025-04-13 15:13:45 +0200

    Notes on Reading Environment Values

--- SwiftUIChronicles/ThinkingInSwiftUI/Environment/Environment.swift
+++ SwiftUIChronicles/ThinkingInSwiftUI/Environment/Environment.swift
@@ -56,6 +56,9 @@ import SwiftUI
 /// point in the view tree, we can change the value for a key by writing into the
 /// environment. The subtree beneath the environment writing modifier will then
 /// receive that modified environment.
+///
+/// >Tip: The environment is also used to make “global” settings such as the current
+/// locale or the time zone available to every view in the app.
 struct EnvironmentContentView: View {
     var body: some View {
         HStack {
@@ -70,6 +73,59 @@ struct EnvironmentContentView: View {
     }
 }
 
+// MARK: - Reading from the Environment
+
+/// We can read from the environment using the `@Environment` property wrapper.
+/// This gives access to a specific value in the environment and obsersves the
+/// environment for any changes to that value. This means that in SwiftUI, any time an
+/// environment value, for example current locale, changes, all views that read
+/// that value will be re-rendered. So, we don't need extra mechanisms to
+/// auto-update the current locale.
+///
+/// >Important: Note that the content view will rerender any time the observed
+/// environment value changes. So, it is important to be careful about the granularity
+/// of the environment values we use for better performance. See the example below.
+///
+/// >Important: Similar to `@State` properties, it doesn’t make sense to expose
+/// `@Environment` properties to the outside. Therefore, we typically mark them
+/// as private and use a linting rule to verify that they’re private. Another
+/// similarity to state properties is that we can only read from the environment
+/// inside the body of a view; we cannot read from the environment inside
+/// a view’s initializer, as the view doesn’t yet have identity at that point.
+/// When we do try to read an environment property in the view’s initializer,
+/// we’ll get a runtime warning.
+struct ReadingFromEnvironmentContentView: View {
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
+    @Environment(\.dynamicTypeSize.isAccessibilitySize) private var isAccessibilitySize
+
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+            if dynamicTypeSize > .large {
+                Image(systemName: "hand.raised.fill")
+                    .foregroundColor(.green)
+            }
+            if isAccessibilitySize {
+                Image(systemName: "hand.raised.fill")
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+// MARK: - Previews
+
 #Preview {
     EnvironmentContentView()
+}
+
+#Preview("Reading from Environment") {
+    ReadingFromEnvironmentContentView()
+    // We can either set the environment value here or click on
+    // the `Variants` button in the rendered preview
+        .environment(\.dynamicTypeSize, .xLarge)
+    ReadingFromEnvironmentContentView()
+    // We can either set the environment value here or click on
+    // the `Variants` button in the rendered preview
+        .environment(\.dynamicTypeSize, .accessibility3)
 }
