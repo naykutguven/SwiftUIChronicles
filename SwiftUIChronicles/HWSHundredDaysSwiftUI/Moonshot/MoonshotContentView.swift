ce597d93d7ad2addc827a6a06bf8ceb0cf0a1a29
Aykut Güven <naykutguven@gmail.com>
2025-04-27 11:28:32 +0200

    HWS Project 8 part 2

--- SwiftUIChronicles/HWSHundredDaysSwiftUI/Moonshot/MoonshotContentView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/Moonshot/MoonshotContentView.swift
@@ -8,9 +8,135 @@
 import SwiftUI
 
 struct MoonshotContentView: View {
+    private let astronauts: [String: Astrounaut] = Bundle.main.decode("astronauts.json")
+    private let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+
     var body: some View {
-        Text("Hello, Moonshot!")
-            .font(.largeTitle)
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        // Using `label` here allows us to show a custom view
+                        // instead of a simple text to navigate users to the detail
+                        // screen
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
+// MARK: - Data Models
+
+private struct Astrounaut: Identifiable, Codable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+private struct Crew: Codable {
+    let name: String
+    let role: String
+}
+
+private struct Mission: Identifiable, Codable {
+    let id: Int
+    let launchDate: Date?
+    let crew: [Crew]
+    let description: String
+
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+}
+
+// MARK: - Generic Bundle Extension
+
+private extension Bundle {
+    func decode<T: Decodable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        // Tedious error handling...
+        do {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "y-MM-dd"
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .formatted(formatter)
+            return try decoder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle. Key '\(key)' not found: \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            print(context.debugDescription)
+            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+    }
+}
+
+// MARK: - Color extension
+
+private extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
     }
 }
 
