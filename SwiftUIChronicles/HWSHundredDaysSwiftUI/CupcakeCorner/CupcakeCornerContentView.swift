c1941696e24951026ef4cf4d1bcc07e1fd7c72c6
Aykut Güven <naykutguven@gmail.com>
2025-04-28 16:39:55 +0200

    HWS Project 10 part 2

--- SwiftUIChronicles/HWSHundredDaysSwiftUI/CupcakeCorner/CupcakeCornerContentView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/CupcakeCorner/CupcakeCornerContentView.swift
@@ -8,9 +8,68 @@
 import SwiftUI
 
 struct CupcakeCornerContentView: View {
+    @State private var order = Order()
+
     var body: some View {
-        Text("Hello, Cupcake Corner!")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+
+                Section {
+                    NavigationLink("Delivery details") {
+                        AddressView(order: order)
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
+}
+
+private struct AddressView: View {
+    var order: Order
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+// MARK: - Data Models
+
+@Observable
+private class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    var type = 0
+    var quantity = 3
+
+    var specialRequestEnabled = false {
+        didSet {
+            if !specialRequestEnabled {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
 }
 
 // MARK: - Previews
--- SwiftUIChronicles/HWSHundredDaysSwiftUI/CupcakeCorner/CupcakeCornerLearningsContentView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/CupcakeCorner/CupcakeCornerLearningsContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Aykut Güven on 28.04.25.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct CupcakeCornerLearningsContentView: View {
@@ -101,6 +102,103 @@ private struct DisableFormContentView: View {
     }
 }
 
+// MARK: - Conforming Codable in Observable classes
+
+@Observable
+private class BrokenObservableCodableUser: Codable {
+    var name = "Aykut"
+}
+
+@Observable
+private class CorrectObservableCodableUser: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+
+    var name = "Aykut"
+}
+
+private struct ObservableCodableContentView: View {
+    var body: some View {
+        Button("Encode broken observable", action: encodeUser)
+        Button("Encode correct observable", action: encodeCorrectUser)
+    }
+
+    func encodeUser() {
+        let data = try! JSONEncoder().encode(BrokenObservableCodableUser())
+        let str = String(decoding: data, as: UTF8.self)
+        // Prints {"_name":"Aykut","_$observationRegistrar":{}} because
+        // Observable macro is rewriting the class behind the scenes.
+        // This can create problems when we send this data to a server.
+        // The server doesn't expect the underscore prefixed keys. So, we need
+        // to define our own CodingKeys enum to avoid this.
+        print(str)
+    }
+
+    func encodeCorrectUser() {
+        let data = try! JSONEncoder().encode(CorrectObservableCodableUser())
+        let str = String(decoding: data, as: UTF8.self)
+        // Prints {"name":"Aykut"} only
+        print(str)
+    }
+}
+
+// MARK: - Haptics
+
+private struct HapticFeedbackContentView: View {
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+
+    var body: some View {
+        Button("Tap count: \(counter)") {
+            counter += 1
+        }
+        .sensoryFeedback(.increase, trigger: counter)
+
+        // More advanced stuff
+        Button("Complex haptics ") {
+            complexSuccess()
+        }
+        // This is necessary to prepare the haptic engine.
+        .onAppear(perform: prepareHaptics)
+    }
+
+    private func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("Failed to create haptic engine: \(error)")
+        }
+    }
+
+    // Advanced haptics
+    private func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        var events = [CHHapticEvent]()
+
+        // You can add multiple events to create a complex pattern.
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(
+            eventType: .hapticTransient,
+            parameters: [intensity, sharpness],
+            relativeTime: 0
+        )
+        events.append(event)
+
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play haptic pattern: \(error)")
+        }
+    }
+}
+
 // MARK: - Data Models
 
 private struct Response: Codable {
@@ -125,4 +223,12 @@ private struct Result: Codable {
 
 #Preview("Disable Form") {
     DisableFormContentView()
+}
+
+#Preview("Observable Codable") {
+    ObservableCodableContentView()
+}
+
+#Preview("Haptic Feedback") {
+    HapticFeedbackContentView()
 }
