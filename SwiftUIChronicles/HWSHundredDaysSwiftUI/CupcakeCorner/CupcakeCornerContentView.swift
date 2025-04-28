10ce727c64db64d2240d8f0056f20aa48befb095
Aykut Güven <naykutguven@gmail.com>
2025-04-28 23:42:44 +0200

    HWS Project 10 part 3

--- SwiftUIChronicles/HWSHundredDaysSwiftUI/CupcakeCorner/CupcakeCornerContentView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/CupcakeCorner/CupcakeCornerContentView.swift
@@ -44,17 +44,116 @@ struct CupcakeCornerContentView: View {
 }
 
 private struct AddressView: View {
-    var order: Order
+    @Bindable var order: Order
 
     var body: some View {
-        Text("Hello World")
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+private struct CheckoutView: View {
+    let order: Order
+
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                let imageUrl = URL(string: "https://hws.dev/img/cupcakes@3x.jpg")
+                AsyncImage(url: imageUrl, scale: 3) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        // This can be useful in prod to avoid the scroll bounce effect.
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+
+    private func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+
+        // it lets us send any data we want, and will automatically send it back.
+        // This is a great way of prototyping network code, because you’ll get
+        // real data back from whatever you send.
+        guard let url = URL(string: "https://reqres.in/api/cupcakes") else {
+            print("Failed to create URL")
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+        }
     }
 }
 
 // MARK: - Data Models
 
 @Observable
-private class Order {
+private class Order: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
 
     var type = 0
@@ -68,8 +167,38 @@ private class Order {
             }
         }
     }
+
     var extraFrosting = false
     var addSprinkles = false
+
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+
+    var hasValidAddress: Bool {
+        !(name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty)
+    }
+
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+
+        // complicated cakes cost more
+        cost += Decimal(type) / 2
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+
+        return cost
+    }
 }
 
 // MARK: - Previews
