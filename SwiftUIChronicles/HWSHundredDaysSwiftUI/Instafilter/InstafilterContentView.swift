2e0c4ac1f75d2928930dfc648d790a7f5cc89136
Aykut Güven <naykutguven@gmail.com>
2025-05-01 19:26:47 +0200

    HWS Project 13 part 4

--- SwiftUIChronicles/HWSHundredDaysSwiftUI/Instafilter/InstafilterContentView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/Instafilter/InstafilterContentView.swift
@@ -5,13 +5,90 @@
 //  Created by Aykut Güven on 01.05.25.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct InstafilterContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
+
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView(
+                            "No picture",
+                            systemImage: "photo.badge.plus",
+                            description: Text("Tap to import a photo")
+                        )
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+
+                    Spacer()
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+
+    private func changeFilter() {
+
+    }
+
+    private func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self),
+                  let inputImage = UIImage(data: imageData)
+            else { return }
+
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+
+            applyProcessing()
+        }
+    }
+
+    private func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage,
+              let cgImage = context.createCGImage(outputImage, from: outputImage.extent)
+        else { return }
+
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
+
+// MARK: - Previews
 
 #Preview {
     InstafilterContentView()
