693d12d48609e6c9b025485a8cc72eccdc8b9d04
Aykut Güven <naykutguven@gmail.com>
2025-05-02 17:03:11 +0200

    HWS Project 14 part 4

--- SwiftUIChronicles/HWSHundredDaysSwiftUI/BucketList/BucketListContentView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/BucketList/BucketListContentView.swift
@@ -66,6 +66,10 @@ struct BucketListContentView: View {
 // MARK: - LocationEditView
 
 private struct LocationEditView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+
     @Environment(\.dismiss) private var dismiss
     var location: BucketItemLocation
     var onSave: (BucketItemLocation) -> Void
@@ -73,12 +77,32 @@ private struct LocationEditView: View {
     @State private var name: String = ""
     @State private var description: String = ""
 
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [WikiPage]()
+
     var body: some View {
         NavigationStack {
             Form {
                 Section {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+
+                Section("Nearby…") {
+                    switch loadingState {
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("\(page.description)")
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading…")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationTitle("Place details")
@@ -92,6 +116,9 @@ private struct LocationEditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
 
@@ -102,6 +129,29 @@ private struct LocationEditView: View {
 
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+
+    private func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // we got some data back!
+            let items = try JSONDecoder().decode(WikiResult.self, from: data)
+
+            // success – convert the array values to our pages array
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
+        }
     }
 }
 
@@ -132,6 +182,28 @@ private struct BucketItemLocation: Codable, Equatable, Identifiable {
     // Equatable
     static func ==(lhs: BucketItemLocation, rhs: BucketItemLocation) -> Bool {
         lhs.id == rhs.id
+    }
+}
+
+private struct WikiResult: Codable {
+    let query: WikiQuery
+}
+
+private struct WikiQuery: Codable {
+    let pages: [Int: WikiPage]
+}
+
+private struct WikiPage: Codable, Comparable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+
+    var description: String {
+        terms?["description"]?.first ?? ""
+    }
+
+    static func < (lhs: WikiPage, rhs: WikiPage) -> Bool {
+        lhs.title < rhs.title
     }
 }
 
