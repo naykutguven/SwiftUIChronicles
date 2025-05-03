fe0b2d57646c37c06de2c43e64c6737019c564bc
Aykut Güven <naykutguven@gmail.com>
2025-05-03 19:43:28 +0200

    HWS Project 16 part 5

--- SwiftUIChronicles/HWSHundredDaysSwiftUI/HotProspects/MeView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/HotProspects/MeView.swift
@@ -5,11 +5,48 @@
 //  Created by Aykut Güven on 03.05.25.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @AppStorage("name") private var name = "Anonymous"
+    @AppStorage("emailAddress") private var email = "you@yoursite.com"
+
+    private let context = CIContext()
+    private let filter = CIFilter.qrCodeGenerator()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+
+                TextField("Email address", text: $email)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+
+                Image(uiImage: generateQRCode(from: "\(name)\n\(email)"))
+                    // needed to prevent blurriness because the QR code is
+                    // generated at a much lower resolution
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+
+    private func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        guard let outputImage = filter.outputImage,
+              let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return UIImage(systemName: "xmark.circle") ?? UIImage()
+        }
+
+        return UIImage(cgImage: cgImage)
     }
 }
 
--- SwiftUIChronicles/HWSHundredDaysSwiftUI/HotProspects/ProspectsView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/HotProspects/ProspectsView.swift
@@ -5,6 +5,8 @@
 //  Created by Aykut Güven on 03.05.25.
 //
 
+
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -15,6 +17,9 @@ struct ProspectsView: View {
 
     @Environment(\.modelContext) private var modelContext
     @Query(sort: \Prospect.name) private var prospects: [Prospect]
+
+    @State private var isShowingScanner = false
+    @State private var selectedProspects = Set<Prospect>()
 
     let filter: FilterType
 
@@ -31,7 +36,7 @@ struct ProspectsView: View {
 
     var body: some View {
         NavigationStack {
-            List(prospects) { prospect in
+            List(prospects, selection: $selectedProspects) { prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
@@ -39,14 +44,57 @@ struct ProspectsView: View {
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
-
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(prospect)
+                    }
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    } else {
+                        Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                    }
+                }
+                // To help SwiftUI understand that each row in our List
+                // corresponds to a single prospect, it's important to add
+                // a tag after the swipe actions
+                .tag(prospect)
             }
             .navigationTitle(title)
             .toolbar {
-                Button("Scan", systemImage: "qrcode.viewfinder") {
-                    let prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
-                    modelContext.insert(prospect)
+                ToolbarItem(placement: .topBarTrailing) {
+                    // We can't have a button that is not wrapped inside
+                    // a ToolbarItem when we have other ToolbarItems.
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
+                    }
                 }
+
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+
+                if selectedProspects.isEmpty == false {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Delete Selected", systemImage: "trash", action: delete)
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                // This is provided by Paul Hudson of HWS.
+                // We could use AVFoundation as well but he says it is
+                // too complicated for this simple task.
+                // Regardless, We also need to add a privacy key to Info.plist
+                CodeScannerView(
+                    codeTypes: [.qr],
+                    simulatedData: "Paul Hudson\npaul@hackingwithswift.com",
+                    completion: handleScan
+                )
             }
         }
     }
@@ -65,6 +113,30 @@ struct ProspectsView: View {
                 },
                 sort: [SortDescriptor(\Prospect.name)]
             )
+        }
+    }
+
+    private func handleScan(result: Result<ScanResult, ScanError>) {
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect(
+                name: details[0],
+                emailAddress: details[1],
+                isContacted: false
+            )
+
+            modelContext.insert(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+
+    private func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
         }
     }
 }
