9fde6c9bfb9726016af655286b4cf2e56d60288c
Aykut Güven <naykutguven@gmail.com>
2025-05-05 10:33:00 +0200

    HWS Project 18 part 2

--- SwiftUIChronicles/HWSHundredDaysSwiftUI/LayoutAndGeometry/LayoutAndGeometryContentView.swift
+++ SwiftUIChronicles/HWSHundredDaysSwiftUI/LayoutAndGeometry/LayoutAndGeometryContentView.swift
@@ -80,6 +80,146 @@ private struct ModifierOrderContentView: View {
     }
 }
 
+// MARK: - GeometryReader
+
+private struct GeometryReaderExamplesContentView: View {
+    var body: some View {
+        GeometryReader { proxy in
+            Image(.kevinHorstmann141705)
+                .resizable()
+                .scaledToFit()
+                .frame(width: proxy.size.width * 0.8)
+            // We could use containerRelativeFrame for some purposes but
+            // it is not always what we want. GeometryReader is more flexible.
+        }
+
+        // Check out the example below.
+        //
+        // containerRelativeFrame looks at the nearest container which is not
+        // the HStack... but the whole screen. BUT its alignment is still in HStack
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+
+            Image(.kevinHorstmann141705)
+                .resizable()
+                .scaledToFit()
+                .containerRelativeFrame(.horizontal) { size, _ in
+                    size * 0.8
+                }
+        }
+
+        // If we use GeometryReader instead, it will take the size that is
+        // proposed to it into account. THe default alignment inside the GeometryReader
+        // is the top left corner. We can solve this by adding another frame modifier.
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+
+            GeometryReader { proxy in
+                Image(.kevinHorstmann141705)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8)
+                    // Centers the content inside the GeometryReader
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+            }
+        }
+    }
+}
+
+private struct GeometryReaderSecondExampleContentView: View {
+    var body: some View {
+        VStack {
+            // takes up all of the proposed space. It is a preferred size, not an absolute size.
+            GeometryReader { proxy in
+                Text("Hello, world!")
+                    .frame(width: proxy.size.width * 0.8)
+                    .background(.red)
+            }
+            .background(.green)
+
+            Text("More text")
+                .background(.blue)
+        }
+    }
+}
+
+// MARK: - Some ScrollView effects using GeometryReader
+
+private struct ScrollViewWithGeometryReaderContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullViewProxy in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % colors.count])
+                            // Creates a spinning helix effect
+                            .rotation3DEffect(
+                                .degrees(proxy.frame(in: .global).minY - fullViewProxy.size.height / 2) / 5,
+                                axis: (x: 0, y: 1.0, z: 0)
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+
+        // Cover flow effect
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { proxy in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Much easier ways to achieve neat scroll effects
+
+private struct EasyScrollViewEffectsContentView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    Text("Number \(num)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        // So much easier
+                        .visualEffect { content, proxy in
+                            content
+                                .rotation3DEffect(
+                                    .degrees(-proxy.frame(in: .global).minX) / 8,
+                                    axis: (x: 0, y: 1, z: 0)
+                                )
+                        }
+                }
+            }
+            // Making each view inside the HStack a scroll target
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
+    }
+}
+
 // MARK: - Previews
 
 #Preview {
@@ -88,4 +228,20 @@ private struct ModifierOrderContentView: View {
 
 #Preview("Modifier Order") {
     ModifierOrderContentView()
+}
+
+#Preview("Geometry Reader") {
+    GeometryReaderExamplesContentView()
+}
+
+#Preview("Geometry Reader 2nd example") {
+    GeometryReaderSecondExampleContentView()
+}
+
+#Preview("ScrollView effects with GeometryReader") {
+    ScrollViewWithGeometryReaderContentView()
+}
+
+#Preview("Easy ScrollView effects") {
+    EasyScrollViewEffectsContentView()
 }
